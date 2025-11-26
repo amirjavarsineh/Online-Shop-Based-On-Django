7abb4149df5a3fb5908af8fa8b17281d53e19d9a
amirjavarsineh <amirjavarsineh7@gmail.com>
2025-11-27 00:30:13 +0330

    Creating catalog templates

--- myshop/shop/urls.py
+++ myshop/shop/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-
+from django.conf.urls.static import static
+from django.conf import settings
 from . import views
 
 app_name = 'shop'
@@ -17,3 +18,7 @@ urlpatterns = [
         name='product_detail',
     ),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
